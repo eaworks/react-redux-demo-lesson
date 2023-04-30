e083767ab942bb6d644055db96648e18fb5cd837
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-30 10:28:05 +0300

    data duzeltmeleri

--- src/CourseItems.js
+++ src/CourseItems.js
@@ -3,28 +3,28 @@ const courseItems = [
         id: 'asdasdasdasd',
         title: 'C programlama eğitimi',
         price: '69',
-        img: 'images/angular.jpg',
+        img: 'https://www.canboz.com/Assets/img/canboz/c.png',
         quantity: 1
     },
     {
         id: 'asdasdasdasdasd',
         title: 'C# Eğitimi',
         price: '69',
-        img: '',
+        img: 'https://www.canboz.com/Assets/img/canboz/csharp.png',
         quantity: 1
     },
     {
         id: 'asdasdaswerewqdasd',
         title: 'Typescript Eğitimi',
         price: '69',
-        img: '',
+        img: 'https://www.canboz.com/Assets/img/canboz/typescript.png',
         quantity: 1
     },
     {
         id: 'asdasdasdfghfgasd',
         title: 'React Eğitimi',
         price: '69',
-        img: '',
+        img: 'https://www.canboz.com/Assets/img/canboz/react.png',
         quantity: 1
     },
 ]
